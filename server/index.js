e1be4001f871368c187a1f639dd2c11fe5302bf0
fran8narf <srlk_doblev@hotmail.com>
2017-08-07 16:41:50 +0200

    express was loaded | the app was initialized | server with HTTP was initialized | socket.io was added to the server.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
-console.log("HOLA MUNDO!");
+var express = require('express'); //cargamos express
+
+var app = express();
+var server = require('http').Server(app); //al método http le decimos que nuestro servidor va a ser la variable en la cual hemos inicializado express.
+
+var io = require('socket.io').Server(server) //aqui hacemos lo mismo que arriba pero la pasamos la variable server que ya lleva la inicialización de express.
+
