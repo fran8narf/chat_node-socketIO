7c21e2b5c59f966eccd5d48a80461bf5dd6e85a5
fran8narf <srlk_doblev@hotmail.com>
2017-08-07 18:09:05 +0200

    Server is now initializing

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ var express = require('express'); //cargamos express
 var app = express();
 var server = require('http').Server(app); //al método http le decimos que nuestro servidor va a ser la variable en la cual hemos inicializado express.
 
-var io = require('socket.io').Server(server) //aqui hacemos lo mismo que arriba pero la pasamos la variable server que ya lleva la inicialización de express.
+var io = require('socket.io')(server) //aqui hacemos lo mismo que arriba pero la pasamos la variable server que ya lleva la inicialización de express.
 
-server.listen( '3020', function(){
+server.listen( 3020, function(){
 	console.log("Server is working properly in http:localhost:3020");
 });
