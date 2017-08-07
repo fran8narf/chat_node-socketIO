c2619ffbcbda214306fe214550f4415b76405375
fran8narf <srlk_doblev@hotmail.com>
2017-08-07 16:43:45 +0200

    server starts listening on port 3020

--- server/index.js
+++ server/index.js
@@ -5,3 +5,6 @@ var server = require('http').Server(app); //al método http le decimos que nuest
 
 var io = require('socket.io').Server(server) //aqui hacemos lo mismo que arriba pero la pasamos la variable server que ya lleva la inicialización de express.
 
+server.listen( '3020', function(){
+	console.log("Server is working properly in http:localhost:3020");
+});
